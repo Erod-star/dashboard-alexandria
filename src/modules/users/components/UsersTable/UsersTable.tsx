068ada95daae10d373465f7bc62097b84f64b753
Miguel <josiacuevas692@gmail.com>
2024-09-23 19:59:46 -0600

    fix(EmptyComponent): Empty Component created and implemented

--- src/modules/users/components/UsersTable/UsersTable.tsx
+++ src/modules/users/components/UsersTable/UsersTable.tsx
@@ -147,7 +147,7 @@ export function UsersTable<TData, TValue>({
                   colSpan={columns.length}
                   className="h-24 text-center"
                 >
-                  No results.
+                  Empty
                 </TableCell>
               </TableRow>
             )}
